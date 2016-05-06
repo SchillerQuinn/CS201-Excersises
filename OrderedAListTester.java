02b2355632183a2c1519d29785a578e566dd18f2
AndrewMaris <andrew.3.maris@gmail.com>
2016-05-05 22:23:27 -0500

    Update OrderedAListTester.java

--- OrderedAListTester.java
+++ OrderedAListTester.java
@@ -1,46 +1,64 @@
 public class OrderedAListTester{
 
 	public static void main(String[] args){
-		OrderedAList<String> test = new OrderedAList<String>();
+		StringComparator sComp = new StringComparator();
+		OrderedAList<String> test = new OrderedAList<String>(sComp);
   		
 		/*******************************
 			Testing the add, contains, get, remove method
-			Should print:
+			Should print unless commented out:
 			
-			EmptyStackException
+			//EmptyStackException
+			//EmptyStackException
+			5
 			true
 			false
 			a
-			IndexOutofRangeException
+			b
+			c
+			d
+			e
+			//IndexOutOfBException
 			a
 			null
-			null
-			null
+			4
+			false
+			false
 			b
+			c
 			0
 			true
 
 		**********************************/
-  		test.remove("meme");
-  		test.contains("meme");
+  		//test.remove("meme");
+  		//test.contains("meme");
 
   		test.add("b");
   		test.add("a");
-  		test.add("r");
+  		test.add("d");
+  		test.add("c");
+  		test.add("e");
+  		System.out.println(test.size());
 
   		System.out.println(test.contains("a"));
   		System.out.println(test.contains("foo")); 
   		
   		System.out.println(test.get(0)); 
-  		System.out.println(test.get(10)); 
+  		System.out.println(test.get(1)); 
+  		System.out.println(test.get(2)); 
+  		System.out.println(test.get(3)); 
+  		System.out.println(test.get(4)); 
+  		//System.out.println(test.get(10)); 
 
 
   		System.out.println(test.remove("a"));
   		System.out.println(test.remove("foo"));
+  		System.out.println(test.size());
   		System.out.println(test.contains("foo"));
   		System.out.println(test.contains("a")); 
 
   		System.out.println(test.get(0));
+  		System.out.println(test.get(1));
 
   		test.clear();
 
